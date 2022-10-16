e3fad5cadfe6f835364d6ddff68efa4939af9627
Chirag Doctor <chiragdoctor@Chirags-MacBook-Pro-2.local>
2022-10-16 13:04:35 +0100

    Add fonts and homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+      roboto: ["Roboto", "sans-serif"]
+    },
     extend: {}
   },
   plugins: []
